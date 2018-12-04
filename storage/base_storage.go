54fe24ac4d284ca6271801cbc71c86e0e9845c7a
Petro Knysh <peteknysh@gmail.com>
2018-12-05 01:02:50 +0200

    storage improvements

--- storage/base_storage.go
+++ storage/base_storage.go
@@ -1,40 +1,14 @@
 package storage
 
-import (
-	"briefExporter/configuration"
-	"database/sql"
-	"log"
-)
-
 type User struct {
 	Email string
 }
 
 type UserConfiguration struct {
+	ScanFolder string
 }
 
 type Storage interface {
-	SaveUserCredentials() error
+	SaveUserCredentials(userName *string) error
 	SaveUserConfiguration(configuration *UserConfiguration) error
-}
-
-func InitStorage(config *configuration.Config) (*sql.DB, error) {
-	db, err := sql.Open("sqlite3", config.PathToLocalDb)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	defer db.Close()
-
-	return db, err
-}
-
-func initScheme(db *sql.DB) error {
-	sqlStmt := `
-	create table user (id guid not null primary key, name text);
-	delete from user;
-	`
-	_, err := db.Exec(sqlStmt)
-
-	return err
 }
--- storage/sqllite_storage.go
+++ storage/sqllite_storage.go
@@ -1,17 +1,52 @@
 package storage
 
 import (
+	"briefExporter/configuration"
+	"database/sql"
 	_ "github.com/mattn/go-sqlite3"
+	"log"
 )
 
 type SqlLiteStorage struct{}
 
-func (s *SqlLiteStorage) SaveUserCredentials() error {
+func (s *SqlLiteStorage) SaveUserCredentials(userName *string) error {
 
 	return nil
 }
 
 func (s *SqlLiteStorage) SaveUserConfiguration(configuration *UserConfiguration) error {
+
+	return nil
+}
+
+func (s *SqlLiteStorage) InitStorage(config *configuration.Config) (*sql.DB, error) {
+	db, err := sql.Open("sqlite3", config.PathToLocalDb)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer db.Close()
+
+	return db, err
+}
+
+func (s *SqlLiteStorage) initScheme(db *sql.DB) error {
+	sqlStmt := `
+	create table user (id guid not null primary key, name text);
+	delete from user;
+	`
+	_, err := db.Exec(sqlStmt)
+
+	return err
+}
+
+func initUserCredentialsTable() error {
+
+	return nil
+}
+
+func initUserConfigurationsTable() error {
+
 
 	return nil
 }
